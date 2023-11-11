76b4b04f02649d29923fd33f25c5fbefd5dc182c
Hasan-aga <careerforma@gmail.com>
2023-11-11 17:05:32 +0300

    create file

---
+++ src/main/java/org/example/FileCreator.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class FileCreator {
+    // gets info
+    // creates post file and fills it
+    // creates assets directory
+    public static void createPostFile(String title, String category) {
+        System.out.println(System.getProperty("user.dir"));
+    }
+}
